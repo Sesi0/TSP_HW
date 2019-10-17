8a18b4d7960c1d857e7fe8adc4ef4f301e55bf20
Borislav Nikolov <borislav.nikolov@petroffsoft.bg>
2019-10-17 17:47:14 +0300

    Added Task3MainForm

---
+++ Lab1_HW/Lab3_HW/Task3MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_HW
+{
+    public partial class Task3MainForm : Form
+    {
+        public Task3MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
